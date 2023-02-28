052c31c471ab323ca1ef7cd0d619395ed1989dba
yanir75 <yanircohen75@gmail.com>
2023-03-01 00:08:51 +0200

    modified outputs

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "aggregator_arn" {
   value = aws_config_configuration_aggregator.account.arn
-  description = "Aggregator arn"
+  description = "AWS config Aggregator arn"
 }
 
 output "authorization_arns" {
-  description = "Authorization arns"
+  description = "AWS config Authorization arns"
   value = [for auth in aws_config_aggregate_authorization.account: auth.arn]
 }
 
 output "account_id" {
-  description = "Account id"
+  description = "AWS Account id"
   value = local.account_id
 }
 
 output "regions" {
-  description = "Regions which the aggregator utilize"
+  description = "Regions which the AWS aggregator utilize"
   value = local.regions
 }
 
 output "ses_email_arn" {
-  description = "SES email arn"
+  description = "Simple Email Service (SES) arn"
   value = aws_ses_email_identity.daily_report_email.arn
 }
 
 output "ses_email" {
-  description = "SES email"
+  description = "SES sender email"
   value = aws_ses_email_identity.daily_report_email.email
 }
 
@@ -44,31 +44,31 @@ output "lambda_module" {
 }
 
 output "role_arn_recorder" {
-  description = "Recorder policy"
+  description = "AWS config Recorder policy"
   value = aws_iam_role.recorder.arn
 }
 
 output "role_arn_policy_id" {
-  description = "Recorder policy"
+  description = "AWS config Recorder policy"
   value = aws_iam_role_policy.recorder.id
 }
 
 output "recorder_name" {
-  description = "Recorder arn"
+  description = "AWS config Recorder arn"
   value = aws_config_configuration_recorder.recorder.name
 }
 
 output "s3_bucket" {
-  description = "s3 bucket name"
+  description = "s3 bucket name AWS config modfications"
   value = aws_s3_bucket.recorder_bucket.bucket
 }
 
 output "s3_bucket_arn" {
-  description = "s3 bucket name"
+  description = "s3 bucket arn for AWS config modfications"
   value = aws_s3_bucket.recorder_bucket.arn
 }
 
 output "recorder_delivery_channel" {
-  description = "recorder delivery channel"
+  description = "AWS config recorder delivery channel"
   value = aws_config_delivery_channel.s3_delivery_chanel.id
 }
